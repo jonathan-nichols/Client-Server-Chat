ceb01d19bef84de6e6ac49f97d9e5b70f5c3b971
jonathan-nichols <jnichols@sandiego.edu>
2021-12-05 17:41:16 -0800

    Implement client class

--- client.py
+++ client.py
@@ -1,25 +1,51 @@
 import socket
+import struct
+
+class Client:
+    def __init__(self) -> None:
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self, host='127.0.0.1', port=8001):
+        self.socket.connect((host, port))
+
+    def receive(self):
+        recv_size = struct.unpack('i', self.socket.recv(4))[0]
+        response = ''
+        while recv_size > 0:
+            response += self.socket.recv(4096).decode()
+            recv_size -= 4096
+        return response
+
+    def send(self, message):
+        data = message.encode()
+        self.socket.sendall(struct.pack('i', len(data)))
+        self.socket.sendall(data)
+
+    def close(self):
+        self.socket.close()
 
 def main():
-    host = '127.0.0.1'
-    port = 8001
-    # create the socket connection
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        print('Type /q to quit')
-        print('Enter message to send...')
-        while True:
-            msg = ''
-            while not msg:
-                msg = input('>')
-            s.sendall(msg.encode())
-            response = s.recv(4096).decode()
-            # output data to the console
+    # initalize the client
+    client = Client()
+    client.connect()
+    print('Type /q to quit')
+    print('Enter message to send...')
+    while True:
+        # get the data to send
+        message = ''
+        while not message:
+            message = input('>')
+        client.send(message)
+        # receive resposne from server
+        try:
+            response = client.receive()
             print(response)
-            # close the connection on quit
-            if not response or response == '/q':
-                break
-
+        except struct.error:
+            response = ''
+        # close client on quit message
+        if not response or response == '/q':
+            client.close()
+            break
 
 
 if __name__ == '__main__':
